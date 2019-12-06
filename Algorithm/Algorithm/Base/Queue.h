3cb8189268dd55e7278e83cee0386f6beb59b042
like5 <like5@staff.weibo.com>
2019-12-06 13:59:17 +0800

    更新base

---
+++ Algorithm/Algorithm/Base/Queue.h
@@ -0,0 +1,32 @@
+//
+//  Queue.h
+//  Algorithm
+//
+//  Created by like5 on 2019/12/6.
+//  Copyright © 2019 like5. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Queue : NSObject
+
+/**
+
+ */
+- (void)enqueue:(id)anObject;
+
+/**
+ Pop object from the front of the queue.
+ @return object from the front of the queue.
+ */
+- (id)dequeue;
+
+- (id)front;
+
+- (id)back;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Algorithm/Algorithm/Base/Queue.m
@@ -0,0 +1,59 @@
+//
+//  Queue.m
+//  Algorithm
+//
+//  Created by like5 on 2019/12/6.
+//  Copyright © 2019 like5. All rights reserved.
+//
+
+#import "Queue.h"
+
+@interface Queue()
+
+@property (nonatomic, strong) NSMutableArray *queue;
+
+@end
+
+@implementation Queue
+
+-(NSMutableArray *)queue
+{
+    if (!_queue) {
+        _queue = [NSMutableArray new];
+    }
+    return _queue;
+}
+
+
+-(id)dequeue
+{
+    id result = nil;
+    if (self.queue.count > 0) {
+        result = [self.queue objectAtIndex:0];
+        [self.queue removeObjectAtIndex:0];
+    }
+    return result;
+}
+
+-(id)front
+{
+    return self.queue.firstObject;
+}
+
+-(void)enqueue:(id)anObject
+{
+    if (self.queue && anObject) {
+        [self.queue addObject:anObject];
+    }
+}
+
+-(id)back
+{
+    if (self.queue) {
+        return self.queue.lastObject;
+    } else {
+        return nil;
+    }
+}
+
+@end
